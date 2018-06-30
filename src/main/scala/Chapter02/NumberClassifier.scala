5335a4b55a83dd31c7ac80ad1ec5105d8c999979
Kim Inseo <sano307@naver.com>
2018-07-01 02:10:02 +0900

    feat(Chapter02): Functional natural number classifier

---
+++ src/main/scala/Chapter02/NumberClassifier.scala
@@ -0,0 +1,26 @@
+package Chapter02
+
+object NumberClassifier extends App {
+
+  val number1: Int = 6
+  val number2: Int = 18
+
+  println(isPerfect(number1))  // true
+  println(isPerfect(number2))  // false
+
+  def factorsOf(number: Int): Seq[Int] =
+    Seq.range(1, number + 1)
+      .filter(potential => number % potential == 0)
+
+  def aliquotSum(number: Int): Int =
+    factorsOf(number).sum - number
+
+  def isPerfect(number: Int): Boolean =
+    aliquotSum(number) == number
+
+  def isAbundant(number: Int): Boolean =
+    aliquotSum(number) > number
+
+  def isDeficient(number: Int): Boolean =
+    aliquotSum(number) < number
+}
