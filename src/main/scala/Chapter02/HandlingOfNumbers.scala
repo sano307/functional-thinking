78db1193c3d6a1584db13e46097b7ec4aa7d028e
Kim Inseo <sano307@naver.com>
2018-07-04 00:06:39 +0900

    feat(Chapter02): How to use a map

---
+++ src/main/scala/Chapter02/HandlingOfNumbers.scala
@@ -0,0 +1,18 @@
+package Chapter02
+
+object HandlingOfNumbers extends App {
+
+  val numbers: List[Int] = List(
+    1, 2, 3, 4, 5,
+    6, 7, 8, 9, 10
+  )
+
+  // List(2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+  numbers.map(_ + 1)
+
+  // List(2, 4, 6, 8, 10)
+  numbers.map(_ % 2 == 0)
+
+  // List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  List(numbers, numbers).flatMap(_.toList)
+}
