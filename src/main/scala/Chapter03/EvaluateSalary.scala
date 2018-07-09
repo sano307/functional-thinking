43bd5175d20741d499018646e249109a32429112
Kim Inseo <sano307@naver.com>
2018-07-10 01:12:11 +0900

    feat(Chapter03): closure example 1

---
+++ src/main/scala/Chapter03/EvaluateSalary.scala
@@ -0,0 +1,17 @@
+package Chapter03
+
+case class Employee(name: String, salary: Int) {
+  def paidMore(amount: Int): Boolean =
+    this.salary > amount
+
+  def isHighPaid = paidMore(3000000)
+}
+
+object EvaluateSalary extends App {
+
+  val worker1: Employee = Employee("inseo", 4000000)
+  val worker2: Employee = Employee("taku", 2999999)
+
+  println(worker1.isHighPaid)  // true
+  println(worker2.isHighPaid)  // false
+}
