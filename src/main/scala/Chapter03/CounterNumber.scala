db303aed6e02256da999c36a18fee662385928d4
Kim Inseo <sano307@naver.com>
2018-07-11 00:54:54 +0900

    feat(Chapter03): closure example 2

---
+++ src/main/scala/Chapter03/CounterNumber.scala
@@ -0,0 +1,33 @@
+package Chapter03
+
+case class Counter() {
+  var cnt: Int = 0
+
+  def increment = cnt += 1
+  def printCnt = println(cnt)
+}
+
+object CounterNumber extends App {
+
+  val counter1 = Counter()
+  val counter2 = Counter()
+
+  counter1.increment
+  counter1.printCnt  // 1
+
+  counter2.increment
+  counter2.increment
+  counter2.printCnt  // 2
+
+  counter2.increment
+  counter2.increment
+  counter2.increment
+  counter2.increment
+  counter2.printCnt  // 6
+
+  counter1.increment
+  counter1.increment
+  counter1.increment
+  counter1.increment
+  counter1.printCnt  // 5
+}
