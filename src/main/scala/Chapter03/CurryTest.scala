92eb70cb5590502a783f5a428689be0af2ed5faa
Kim Inseo <sano307@naver.com>
2018-07-12 00:55:43 +0900

    feat(Chapter03): Currying example

---
+++ src/main/scala/Chapter03/CurryTest.scala
@@ -0,0 +1,15 @@
+package Chapter03
+
+object CurryTest extends App {
+
+  def filter(xs: List[Int], p: Int => Boolean): List[Int] =
+    if (xs.isEmpty) xs
+    else if (p(xs.head)) xs.head :: filter(xs.tail, p)
+    else filter(xs.tail, p)
+
+  def modN(n: Int)(x: Int) = (x % n) == 0
+
+  val nums = List(1, 2, 3, 4, 5, 6, 7, 8)
+  println(filter(nums, modN(2)))  // List(2, 4, 6, 8)
+  println(filter(nums, modN(3)))  // List(3, 6)
+}
