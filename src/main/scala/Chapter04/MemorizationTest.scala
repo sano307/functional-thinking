a86a4b01bd6e302374eaad810fe74803ab02e23a
Kim Inseo <sano307@naver.com>
2018-07-27 01:19:49 +0900

    feat(Chapter04): Memorization example

---
+++ src/main/scala/Chapter04/MemorizationTest.scala
@@ -0,0 +1,24 @@
+package Chapter04
+
+import scala.collection.mutable.{Map => MutableMap}
+
+object MemorizationTest extends App {
+
+  def memorize[A, B](f: A => B) = new (A => B) {
+    val cache: MutableMap[A, B] = MutableMap[A, B]()
+
+    def apply(x: A): B = cache.getOrElseUpdate(x, f(x))
+  }
+
+  val students: MutableMap[Int, String] = MutableMap(
+    1 -> "inseo",
+    2 -> "taku",
+    3 -> "nomura"
+  )
+
+  def nameHash: Int => String = memorize(students)
+
+  for(cnt <- 1 to students.size) {
+    println(nameHash(cnt))
+  }
+}
